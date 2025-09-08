326616a3e5b936f700f598a05535a6af715d07f0
ESET0140 <taraknath.mahato@esyasoft.com>
2025-09-08 15:41:19 +0530

    day 5

---
+++ Basic Python/day 3/moduledemo.py
@@ -0,0 +1,11 @@
+import calculator
+import math
+
+
+
+print(calculator.add(9,10))
+print(calculator.sub(100,90))
+print(calculator.multiply(40,30))
+print(calculator.square(40,30))
+
+print(math.sqrt(36)) 

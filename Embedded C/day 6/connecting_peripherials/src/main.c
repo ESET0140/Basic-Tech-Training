9b14043ec70e3e5dc86d06f353bad7503a0f6fc6
ESET0140 <taraknath.mahato@esyasoft.com>
2025-09-09 13:21:14 +0530

    day 6 hands on

---
+++ Embedded C/day 6/connecting_peripherials/src/main.c
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <>
+#include <util/delay.h>
+
+#define DATA_PIND7 PD7
+#define DATA_PIND6 PB0
+#define DATA_PIND5 PB1
+#define DATA_PIND4 PB2
+#define ENABLE PB3
+#define DATA_RS PB4
+
+int lcd command(){
+
+    
+}

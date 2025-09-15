e89729cc015e70245e20e3f0a47f6ac96532344f
ESET0140 <taraknath.mahato@esyasoft.com>
2025-09-15 23:02:13 +0530

    Class dem0

---
+++ .NET/class exercise/studrntdemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace classdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student first= new student();
+            first.initialise();
+            first.showDisplay();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ .NET/class exercise/studrntdemo/student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classdemo
+{
+    internal class student
+    {
+        int student_id;
+        string name;
+        int age;
+        string contactno;
+        string emailid;
+
+        public void initialise()
+        {
+            student_id = 1;
+            name = "Tarak";
+            age = 24;
+            contactno = "9709373578";
+            emailid= "taraknathmahatoda@gmail.com";
+        }
+        public void showDisplay()
+        {
+            Console.WriteLine("Displaying Object");
+            Console.WriteLine(name);
+        }
+        public student(int id,string std_name, int age, string contactno, string emailid)
+        {
+            student_id = id;
+            name = std_name;
+        }
+
+        public student() {
+        }
+
+      public void getTotal()
+        {
+
+        }
+        
+    }
+}

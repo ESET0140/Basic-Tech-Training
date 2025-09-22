33c516c1b852bf397517fccd268141ef56bbe9ad
ESET0140 <taraknath.mahato@esyasoft.com>
2025-09-22 23:39:13 +0530

    Assignmemt 2

---
+++ React/Assignment 2/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import MouseTracker from './component/MouseTracker';
+
+
+function App() {
+  return (
+    <div>
+   <MouseTracker/>s
+    </div>
+  );
+}
+
+export default App;

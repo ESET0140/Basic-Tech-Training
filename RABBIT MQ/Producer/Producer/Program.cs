2970597b3c0bb2832585ab6c174ba64a7f82038c
ESET0140 <taraknath.mahato@esyasoft.com>
2025-09-24 11:38:01 +0530

    class assignment

---
+++ RABBIT MQ/Producer/Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using Producer;
+using Producer.Exchanges;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "/", UserName = "guest", Password = "guest" };
+DirectExchange directExchange = new DirectExchange(factory);
+
+Console.WriteLine("Enter messages to send (type 'exit' to quit):");
+
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input)) continue;
+    if (input.ToLower() == "exit") break;
+
+    await directExchange.SendMessage(input);
+
+
+}

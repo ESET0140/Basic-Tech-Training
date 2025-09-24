ed4857153f8f3d0b0f60ad50a1b3d4298f36123a
ESET0140 <taraknath.mahato@esyasoft.com>
2025-09-24 21:53:16 +0530

    Banking system

---
+++ .NET/class exercise/demoabstraction/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace demoabstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingsAccount acc = new SavingsAccount("SB001", "Amit Kumar", 1000.0);
+            acc.DisplayBalance();
+            acc.Deposit(500);
+            acc.Withdraw(200);
+            acc.ApplyInterest();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ .NET/class exercise/demoabstraction/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoabstraction
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount(string accountNumber, string holderName, double balance = 0.0, double interestRate = 0.05)
+            : base(accountNumber, holderName, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void ApplyInterest()
+        {
+            double interest = Balance * InterestRate;
+            Balance += interest;
+            Console.WriteLine($"Interest of ₹{interest} applied. New Balance: ₹{Balance}");
+        }
+    }
+}
